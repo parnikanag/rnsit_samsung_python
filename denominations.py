f17f50b774f03ab676aa76298f19a91d1ed2a059
Parnika Nag <163747214+parnikanag@users.noreply.github.com>
2025-02-23 19:33:54 +0530

    denominations.py

--- denominations.py
+++ denominations.py
@@ -1,1 +1,12 @@
+def min_coins_notes(amount):
+    currency = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]  
+    print("Coins and Notes Required:")
+    for note in currency:
+        if amount >= note:
+            count = amount // note
+            print(note, ":", count)
+            amount %= note  
+
+amount = int(input("Enter the amount: "))
+min_coins_notes(amount)
 
